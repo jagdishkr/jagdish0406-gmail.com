9502713d20e639636c8fdc719f271cfc117b7e3d
jagdish kumar <jagdish0406@gmail.com>
2020-03-30 15:49:02 +0530

    add service file

---
+++ src/app/test-service/test-service.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-service',
+  templateUrl: './test-service.component.html',
+  styleUrls: ['./test-service.component.sass']
+})
+export class TestServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
